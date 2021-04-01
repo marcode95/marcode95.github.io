e39f8b4cffd757e77c3431ee32db61c25596377c
Marco <marcoerhardt95@gmail.com>
2021-04-01 21:12:32 +0200

    Style out portfolio section to a point where it's to almost too fire to show anyone

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ canvas();
 
 const mediaQuery = window.matchMedia('(max-width: 540px)')
 
-
 const handleTabletChange = (e) => {
   if (e.matches) {
     const selfStack = document.querySelectorAll("div.self-stack-check, span.self-stack-name");
@@ -32,5 +31,24 @@ const handleTabletChange = (e) => {
 }
 
 mediaQuery.addListener(handleTabletChange);
-
 handleTabletChange(mediaQuery);
+
+
+
+
+
+document.querySelectorAll('div.project-image-container').forEach(item => {
+  item.addEventListener('mouseenter', (e) => {
+    e.target.innerHTML = `
+    <div class="project-description w-100 h-100 d-flex flex-column justify-content-between align-items-center">
+      <div data-aos="fade-down" data-aos-delay="200" data-aos-duration="500">
+        <p class="project-title">Project</p>
+        <p class="project-stack">Javascript</p>
+      </div>
+      <a href="#" class="project-link" data-aos="fade-up" data-aos-delay="200" data-aos-duration="500" data-aos-anchor=".project-title">LIVE DEMO</a>
+    </div>`;
+  })
+  item.addEventListener('mouseleave', (e) => {
+    e.target.innerHTML = ``;
+  })
+})
