551e29473b974ccc4192d2a20975197235d36e61
Marco <marcoerhardt95@gmail.com>
2021-04-01 11:11:41 +0200

    Fix aos but, no horizontal scrollbar for small devices due to fading in elements there anymore

--- src/index.js
+++ src/index.js
@@ -6,3 +6,36 @@ const showcaseHeight = document.getElementById('showcase').scrollHeight
 showcaseButton.addEventListener('click', () => {
   window.scrollBy(0, window.scrollTo(0, window.innerHeight))
 });
+
+
+
+const mediaQuery = window.matchMedia('(max-width: 540px)')
+
+
+const handleTabletChange = (e) => {
+  if (e.matches) {
+    const selfStack = document.querySelectorAll("div.self-stack-check, span.self-stack-name");
+    for (let i = 0; i < selfStack.length; i++) {
+      let item = selfStack[i];
+      item.setAttribute('data-aos', '')
+    }
+  }
+
+  else {
+    const selfStackName = document.querySelectorAll("span.self-stack-name");
+    const selfStackCheck = document.querySelectorAll("div.self-stack-check");
+    for (let i = 0; i < selfStackName.length; i++) {
+      let item = selfStackName[i];
+      item.setAttribute('data-aos', 'fade')
+    }
+    for (let i = 0; i < selfStackCheck.length; i++) {
+      let item = selfStackCheck[i];
+      item.setAttribute('data-aos', 'fade-left')
+    }
+  }
+}
+
+mediaQuery.addListener(handleTabletChange);
+
+handleTabletChange(mediaQuery);
+
