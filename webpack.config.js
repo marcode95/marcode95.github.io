edd6bc9771369319dfd3a8bffbe82097be8459ca
Marco <marcoerhardt95@gmail.com>
2021-04-24 14:17:23 +0200

    Move index.html to root

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   devServer: {
-    contentBase: './dist',
+    contentBase: './',
   },
   output: {
     filename: 'main.js',
