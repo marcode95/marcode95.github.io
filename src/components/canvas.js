d4690fcd47ac6a7fa7a31140144f13ae8332cdad
Marco <marcoerhardt95@gmail.com>
2021-05-27 16:53:47 +0200

    Update velocity of stars in canvas section

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -127,8 +127,8 @@ const canvas = () => {
 
   function update() {
 
-  velocity.tx *= 0.96;
-  velocity.ty *= 0.96;
+  velocity.tx *= 0.66;
+  velocity.ty *= 0.66;
 
   velocity.x += ( velocity.tx - velocity.x ) * 0.8;
   velocity.y += ( velocity.ty - velocity.y ) * 0.8;
