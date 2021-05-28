af23cbf7fec995401fdd59c400a120a7955dc600
Marco <marcoerhardt95@gmail.com>
2021-05-28 11:46:24 +0200

    Add right project summary to every project, slightly change aos animations, done with project summaries now

--- src/index.js
+++ src/index.js
@@ -36,9 +36,9 @@ const projectImageContainers = document.querySelectorAll('div.project-image-cont
 projectImageContainers[0].addEventListener('mouseenter', (e) => {
   e.target.innerHTML = `
   <div class="project-description w-100 h-100 d-flex flex-column align-items-center">
-    <p class="project-title" data-aos="fade-down" data-aos-delay="200" data-aos-duration="500">IN THE WOODS</p>
-    <p class="project-stack" data-aos="fade-down" data-aos-delay="200" data-aos-duration="550" data-aos-anchor=".project-title">Javascript / Phaser</p>
-    <p class="project-summary" data-aos="fade-down" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title">Sidescroller Jump 'n Run game including shooting enemies, collecting coins, an end boss fight, and a leaderboard API.</p>
+    <p class="project-title" data-aos="fade-down" data-aos-delay="200" data-aos-duration="600">IN THE WOODS</p>
+    <p class="project-stack" data-aos="fade-down" data-aos-delay="300" data-aos-duration="500" data-aos-anchor=".project-title">JavaScript / Phaser</p>
+    <p class="project-summary" data-aos="fade-down" data-aos-delay="400" data-aos-duration="400" data-aos-anchor=".project-title">Sidescroller Jump 'n Run game including shooting enemies, collecting coins, an end boss fight, and a leaderboard API.</p>
     <div class="d-flex">
       <a href="https://agitated-curie-05b9b2.netlify.app/" class="github-link" data-aos="fade-right" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title" target="_blank">LIVE DEMO</a>
       <a href="https://github.com/marcode95/phaser-game" class="github-link" data-aos="fade-left" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title" target="_blank">GITHUB</a>
@@ -52,9 +52,9 @@ projectImageContainers[0].addEventListener('mouseleave', (e) => {
 projectImageContainers[1].addEventListener('mouseenter', (e) => {
   e.target.innerHTML = `
     <div class="project-description w-100 h-100 d-flex flex-column align-items-center">
-      <p class="project-title" data-aos="fade-down" data-aos-delay="200" data-aos-duration="500">E-DEALS</p>
-      <p class="project-stack" data-aos="fade-down" data-aos-delay="200" data-aos-duration="500" data-aos-anchor=".project-title">HTML / CSS / Rails</p>
-      <p class="project-summary" data-aos="fade-down" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title">Sidescroller Jump 'n Run game including shooting enemies, collecting coins, an end boss fight, and a leaderboard API.</p>
+      <p class="project-title" data-aos="fade-down" data-aos-delay="200" data-aos-duration="600">E-DEALS</p>
+      <p class="project-stack" data-aos="fade-down" data-aos-delay="300" data-aos-duration="500" data-aos-anchor=".project-title">HTML / CSS / Rails</p>
+      <p class="project-summary" data-aos="fade-down" data-aos-delay="400" data-aos-duration="400" data-aos-anchor=".project-title">Mobile app for keeping track of the user's spendings. Turn on 'Touch Simulation' in Firefox when checking it out!</p>
       <div class="d-flex">
         <a href="https://desolate-beach-15262.herokuapp.com/" class="github-link" data-aos="fade-right" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title" target="_blank">LIVE DEMO</a>
         <a href="https://github.com/marcode95/e-deals" class="github-link" data-aos="fade-left" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title" target="_blank">GITHUB</a>
@@ -68,9 +68,9 @@ projectImageContainers[1].addEventListener('mouseleave', (e) => {
 projectImageContainers[2].addEventListener('mouseenter', (e) => {
   e.target.innerHTML = `
   <div class="project-description w-100 h-100 d-flex flex-column align-items-center">
-    <p class="project-title" data-aos="fade-down" data-aos-delay="200" data-aos-duration="500">PRO-CON</p>
-    <p class="project-stack" data-aos="fade-down" data-aos-delay="200" data-aos-duration="500" data-aos-anchor=".project-title">HTML / CSS</p>
-    <p class="project-summary" data-aos="fade-down" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title">Sidescroller Jump 'n Run game including shooting enemies, collecting coins, an end boss fight, and a leaderboard API.</p>
+    <p class="project-title" data-aos="fade-down" data-aos-delay="200" data-aos-duration="600">PRO-CON</p>
+    <p class="project-stack" data-aos="fade-down" data-aos-delay="300" data-aos-duration="500" data-aos-anchor=".project-title">HTML / CSS</p>
+    <p class="project-summary" data-aos="fade-down" data-aos-delay="400" data-aos-duration="400" data-aos-anchor=".project-title">HTML&CSS website for selling gaming controllers.</p>
     <div class="d-flex">
       <a href="https://raw.githack.com/marcode95/HTML-CSS-capstone/feature/index.html" class="github-link" data-aos="fade-right" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title" target="_blank">LIVE DEMO</a>
       <a href="https://github.com/marcode95/HTML-CSS-capstone" class="github-link" data-aos="fade-left" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title" target="_blank">GITHUB</a>
@@ -84,9 +84,9 @@ projectImageContainers[2].addEventListener('mouseleave', (e) => {
 projectImageContainers[3].addEventListener('mouseenter', (e) => {
   e.target.innerHTML = `
   <div class="project-description w-100 h-100 d-flex flex-column align-items-center">
-    <p class="project-title" data-aos="fade-down" data-aos-delay="200" data-aos-duration="500">TO-DO'S</p>
-    <p class="project-stack" data-aos="fade-down" data-aos-delay="200" data-aos-duration="500" data-aos-anchor=".project-title">HTML, CSS, Javascript</p>
-    <p class="project-summary" data-aos="fade-down" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title">Sidescroller Jump 'n Run game including shooting enemies, collecting coins, an end boss fight, and a leaderboard API.</p>
+    <p class="project-title" data-aos="fade-down" data-aos-delay="200" data-aos-duration="600">TO-DO'S</p>
+    <p class="project-stack" data-aos="fade-down" data-aos-delay="300" data-aos-duration="500" data-aos-anchor=".project-title">HTML / CSS / JavaScript</p>
+    <p class="project-summary" data-aos="fade-down" data-aos-delay="400" data-aos-duration="400" data-aos-anchor=".project-title">The classic: An app for keeping track of the user's "to-do's" built with JS!</p>
     <div class="d-flex">
       <a href="https://cocky-ardinghelli-805b0e.netlify.app/" class="github-link" data-aos="fade-right" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title" target="_blank">LIVE DEMO</a>
       <a href="https://github.com/marcode95/to-do-list" class="github-link" data-aos="fade-left" data-aos-delay="200" data-aos-duration="600" data-aos-anchor=".project-title" target="_blank">GITHUB</a>
